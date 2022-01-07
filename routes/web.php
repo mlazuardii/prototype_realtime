443ab937f5ec64c998df86b800dc5d28fe080724
lazuardi <muhammadlazuardiimani@gmail.com>
2022-01-07 10:58:20 +0700

    berhasil integrasi dengan pusher

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Events\LoadAgain;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    LoadAgain::dispatch('data diganti');
+
     return view('welcome');
 });
